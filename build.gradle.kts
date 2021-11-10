b3a7c8a6cba0a67e75c778bd63827cd906777f23
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-11-10 15:00:01 +0100

    Add plugin

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("com.javiersc.semver.gradle.plugin") version "0.1.0-alpha.0.16+20211111T151758Z"
+}
+
+semver {
+    tagPrefix.set("v")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal()
+    }
+}
