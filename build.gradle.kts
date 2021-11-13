dc55418864b145a6d3c7ad378864cc6a6bda87c5
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-11-13 18:14:39 +0100

    Update plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.javiersc.semver.gradle.plugin") version "0.1.0-alpha.0.16+20211111T151758Z"
+    id("com.javiersc.semver.gradle.plugin") version "0.1.0-alpha.2"
 }
 
 semver {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,3 +13,5 @@ dependencyResolutionManagement {
         mavenLocal()
     }
 }
+
+include(":library")
