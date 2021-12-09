960a94750c79702458955fde028c6fead4165cc0
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-12-09 22:54:50 +0100

    Update plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
 plugins {
-    id("com.javiersc.semver.gradle.plugin") version "0.1.0-alpha.2"
-}
-
-semver {
-    tagPrefix.set("v")
+    id("com.javiersc.semver.gradle.plugin") version "0.1.0-alpha.6.4+2021-12-09T21-46-38-193831400Z"
 }
